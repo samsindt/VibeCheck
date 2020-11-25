4d6e184ffc6891d4487feb7994920c5e4ce60a86
samsindt <samsground@gmail.com>
2020-11-24 16:32:09 -0800

    Add updated UML to reflect schema changes

--- models/answer.js
+++ models/answer.js
@@ -5,3 +5,9 @@ var AnswerSchema = new mongoose.Schema({
     inResponseTo: {type: mongoose.Schema.Types.ObjectId, ref: 'Question', required: true},
     agreedWithBy: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
 });
+
+UserSchema.methods.getNumAgreements = function(password) {
+    return this.agreedWithBy.length;
+};
+
+module.exports = mongoose.model('Answer', AnswerSchema);
