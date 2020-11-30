fcec6c8b496e622b2a683569fa05e3b264b77be7
SamSindt <samsground@gmail.com>
2020-11-30 11:39:17 -0800

    Add user ID to session cookie payload so that it is easier to retrieve user documents

---
+++ helpers/auth.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config.json');
+
+module.exports = {
+    setJWTCookie: (res, username, userId) => {
+        const expiration = 90000;
+        const token = jwt.sign({username: username, userId: userId}, config.jwtSecretKey);
+        res.cookie('token', token, {
+            expires: new Date(Date.now() + expiration),
+            secure: false,
+            httpOnly: false,
+        });
+    }
+}
