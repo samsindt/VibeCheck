c75822685fea2dfaa065633b1dbb890f9ff69100
Devin Kwon <kwon2505@pacificu.edu>
2020-12-01 16:31:41 -0800

    index page now renders the most popular poll title

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
+const { response } = require('../app');
 var router = express.Router();
+var QuestionModel = require('../models/question');
+var AnswerModel = require('../models/answer');
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'VibeCheck' });
+  QuestionModel.find().populate('responses').exec(function(err, docs) {
+    if (err) {
+      console.error("Error finding all polls by popularity: " + err);
+      return res.sendStatus(500);
+    }
+
+    let responsePayload = [];
+
+    docs.sort((a, b) => b.popularity - a.popularity);
+
+    docs.forEach(question => {
+      responsePayload.push(
+        {
+          text: question.text,
+          id: question._id,
+          numResponses: question.popularity
+        }
+      );
+    });
+
+    // instead of sending json, responsePayload could be used to render a mustache page
+    //res.json(responsePayload);
+    console.log(responsePayload[0]);
+    res.render('index', {title: 'VibeCheck', question: responsePayload[0].text});
+  })
 });
 
 module.exports = router;
