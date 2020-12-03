dc0d0da8e2d6d2c0357e9d0c1e69dd6a439acd25
samsindt <samsground@gmail.com>
2020-12-03 02:20:52 -0800

    Add chart.js boilerplate

---
+++ public/script/poll.js
@@ -0,0 +1,17 @@
+(function() {
+    var ctx = document.getElementById('chart').getContext('2d');
+
+    var chart = new chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45]
+            }]
+        },
+        options: {}
+    });
+})();
