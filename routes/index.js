98e16fa7ec68bd198c02a4161083b0ee1561f002
Devin Kwon <kwon2505@pacificu.edu>
2020-12-02 16:57:10 -0800

    worked on grabbing answers from most popular question

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const { response } = require('../app');
 var router = express.Router();
 var QuestionModel = require('../models/question');
 var AnswerModel = require('../models/answer');
+const { findByIdAndUpdate } = require('../models/question');
 
 router.get('/', function(req, res) {
   QuestionModel.find().populate('responses').exec(function(err, docs) {
@@ -24,8 +25,26 @@ router.get('/', function(req, res) {
         }
       );
     });
-   
+    
+    QuestionModel.findById(responsePayload[0].id, function (err, questionTitle) {
+      
+      AnswerModel.findById(questionTitle.responses[0], function (err, answer){
+        console.log(answer.inResponseTo);
+        AnswerModel.findById("5fc45e1e3cb7d7338c6e2303").populate('agreedWithBy').exec(function(err, docs) {
+        if (err) {
+          console.error("Error finding all polls by popularity: " + err);
+          return res.sendStatus(500);
+        }
+
+        console.log (docs);
+
+      })
+
+      })
+
     res.render('index', {title: 'VibeCheck', question: responsePayload[0].text});
+    });
+    
   })
 });
 
